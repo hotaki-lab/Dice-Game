0e696acaa1aee4f359385ad3cfdb1fd7311437d1
H. Hotaki <67237933+hotaki-lab@users.noreply.github.com>
2021-07-26 11:05:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-@import "styles.css";
-
 var randomNumber1 = Math.floor(((Math.random())*6)+1);
 var img1Rand = "images/dice" + randomNumber1 + ".png"
 var image1 = document.querySelectorAll("img")[0].setAttribute('src', img1Rand);
